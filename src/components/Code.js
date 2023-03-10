78f3cf628a808c408c038053045d70c3ad0b29a5
Aayush <just.another.3d.object@gmail.com>
2023-03-11 00:17:19 +0530

    Added HTML boilerplate in output field

--- src/components/Code.js
+++ src/components/Code.js
@@ -1,7 +1,20 @@
 const Code = () => {
   return (
     <div className="Code">
-      <pre className="broder rounded bg-light p-3">{`Hello`}</pre>
+      <pre className="broder rounded bg-light p-3">{`<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>        
+        <script src="" async defer></script>
+    </body>
+</html>`}</pre>
     </div>
   );
 };
