8b370a6d99684c598dc7290840d6383843c7f0d7
Aayush <just.another.3d.object@gmail.com>
2023-03-11 00:20:48 +0530

    Added meta tags and some links to output HTML

--- src/components/Code.js
+++ src/components/Code.js
@@ -4,12 +4,32 @@ const Code = () => {
       <pre className="broder rounded bg-light p-3">{`<!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="author" content="{FullName}" />
+        <meta property="og:title" content="{FullName}'s Portfolio" />
+        <meta property="og:site_name" content="{FullName}'s Portfolio" />
+        <meta property="og:url" content="{URL}" />
+        <meta property="og:description" content="{Description}" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="{Thumbnail}" />
+        <meta name="title" content="{FullName}'s Portfolio" />
+        <meta name="description" content="{Description}" />
+        <meta name="keywords" content="{KeyWords}" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="{URL}" />
+        <meta property="twitter:title" content="{FullName}'s Portfolio" />
+        <meta property="twitter:description" content="{Description}" />
+        <meta property="twitter:image" content="{Thumbnail}" />
+        <title>Portfolio_AuthorName's Portfolio</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="https://startbootstrap.github.io/startbootstrap-resume/css/styles.css" rel="stylesheet" />
     </head>
     <body>        
         <script src="" async defer></script>
