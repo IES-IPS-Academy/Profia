2fdf4aca8ad9e389b097f7abdf83bf9155f7cf8b
Aayush <just.another.3d.object@gmail.com>
2023-03-11 00:47:03 +0530

    Added code for Experience secttion of output HTML

--- src/components/Code.js
+++ src/components/Code.js
@@ -71,9 +71,47 @@ const Code = () => {
                         <a class="social-icon" href="#!"><i class="fab fa-instagram"></i></a>
                     </div>
                 </div>
-        </section>
+            </section>
         <hr class="m-0" />
             <!-- Experience-->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Experience</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Position</h3>
+                            <div class="subheading mb-3">Company</div>
+                            <p>Job desciption</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Work period</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Position</h3>
+                            <div class="subheading mb-3">Company</div>
+                            <p>Job desciption</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Work period</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Position</h3>
+                            <div class="subheading mb-3">Company</div>
+                            <p>Job desciption</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Work period</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Position</h3>
+                            <div class="subheading mb-3">Company</div>
+                            <p>Job desciption</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Work period</span></div>
+                    </div>
+                </div>
+            </section>
+        <hr class="m-0" />
             <!-- Education-->
             <!-- Skills-->
             <!-- Interests-->
