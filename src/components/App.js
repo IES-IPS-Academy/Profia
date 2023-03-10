0c1a0936ff1fb92edcfb742a0232a2da8ec966c8
Aayush <just.another.3d.object@gmail.com>
2023-03-10 21:55:29 +0530

    Added dark mode toggle button

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
 import Header from "./Bootstrap/Header";
-
 class App extends Component {
+  state = {
+    Dark: false,
+  };
+  toggleHeader = () => {
+    this.setState({
+      Dark: !this.state.Dark,
+    });
+  };
   render() {
     return (
       <div className="App">
-        <Header>Portfolio Generator</Header>
+        <Header dark={this.state.Dark} classsname="mb-5">
+          Portfolio Generator
+          <button
+            className={`btn btn${this.state.Dark ? "-outline" : ""}-primary`}
+            onClick={this.toggleHeader}
+          >
+            Toggle Dark mode
+          </button>
+        </Header>
       </div>
     );
   }
 }
-
 export default App;
