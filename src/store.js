088145173f6af1e22f9a91eaec10b38570fb27d4
Aayush <just.another.3d.object@gmail.com>
2023-03-13 03:27:47 +0530

    Added items to reducers object in store.js

--- src/store.js
+++ src/store.js
@@ -1,5 +1,16 @@
 import { createStore, combineReducers } from "redux";
-const reducers = {};
+import { experiences } from "./components/Form/Experience/reducers";
+import { educations } from "./components/Form/Education/reducers";
+import { awards } from "./components/Form/Awards/reducers";
+import { interests } from "./components/Form/Interests/reducers";
+import { skills } from "./components/Form/Skills/reducers";
+const reducers = {
+  experiences,
+  educations,
+  awards,
+  interests,
+  skills,
+};
 
 const rootReducer = combineReducers(reducers);
 
