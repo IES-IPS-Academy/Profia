3866cd5d65971e6f08cb46439ee7198de6ff46d2
Aayush <just.another.3d.object@gmail.com>
2023-03-13 02:37:39 +0530

    Added reducres for education section

---
+++ src/components/Form/Education/reducers.js
@@ -0,0 +1,24 @@
+// Imported actions for education
+import { CREATE_EDUCATION, REMOVE_EDUCATION } from "./actions";
+
+// Decides what to do with the state when an action happens
+export const educations = (state = [], action) => {
+  const { type, payload } = action;
+
+  // Switch statement to handle creation or removal of education from the form
+  switch (type) {
+    case CREATE_EDUCATION: {
+      const { education } = payload;
+      const newEducation = {
+        education,
+      };
+      return state.concat(newEducation);
+    }
+    case REMOVE_EDUCATION: {
+      const { education } = payload;
+      return state.filter((obj) => obj.education.degree !== education.degree);
+    }
+    default:
+      return state;
+  }
+};
