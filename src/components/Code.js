6cc25f3ddb20de47cb068f423e72709d75e53a09
Aayush <just.another.3d.object@gmail.com>
2023-03-12 23:46:13 +0530

    Removed trailing slashes from output HTML

--- src/components/Code.js
+++ src/components/Code.js
@@ -31,41 +31,41 @@ const Code = ({
   return (
     <div className="Code">
       <pre className="broder rounded bg-light p-3">{`<!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="author" content="${FullName}" />
-        <meta property="og:title" content="${FullName}'s Portfolio" />
-        <meta property="og:site_name" content="${FullName}'s Portfolio" />
-        <meta property="og:url" content="${URL}" />
-        <meta property="og:description" content="${Description}" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="${Thumbnail}" />
-        <meta name="title" content="${FullName}'s Portfolio" />
-        <meta name="description" content="${Description}" />
-        <meta name="keywords" content="${KeyWords}" />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="${URL}" />
-        <meta property="twitter:title" content="${FullName}'s Portfolio" />
-        <meta property="twitter:description" content="${Description}" />
-        <meta property="twitter:image" content="${Thumbnail}" />
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="author" content="${FullName}">
+        <meta property="og:title" content="${FullName}'s Portfolio">
+        <meta property="og:site_name" content="${FullName}'s Portfolio">
+        <meta property="og:url" content="${URL}">
+        <meta property="og:description" content="${Description}">
+        <meta property="og:type" content="website">
+        <meta property="og:image" content="${Thumbnail}">
+        <meta name="title" content="${FullName}'s Portfolio">
+        <meta name="description" content="${Description}">
+        <meta name="keywords" content="${KeyWords}">
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="twitter:url" content="${URL}">
+        <meta property="twitter:title" content="${FullName}'s Portfolio">
+        <meta property="twitter:description" content="${Description}">
+        <meta property="twitter:image" content="${Thumbnail}">
         <title>${FullName}'s Portfolio</title>
-        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico">
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
         <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
-        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css">
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="https://startbootstrap.github.io/startbootstrap-resume/css/styles.css" rel="stylesheet" />
+        <link href="https://startbootstrap.github.io/startbootstrap-resume/css/styles.css" rel="stylesheet">
     </head>
     <body id="page-top">
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
             <a class="navbar-brand js-scroll-trigger" href="#page-top">
                 <span class="d-block d-lg-none">${FullName}</span>
-                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="${Thumbnail}" alt="${FullName}" /></span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="${Thumbnail}" alt="${FullName}"></span>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
@@ -102,7 +102,7 @@ const Code = ({
           </div>
                 </div>
             </section>
-        <hr class="m-0" />
+        <hr class="m-0">
             <!-- Experience-->
             <section class="resume-section" id="experience">
                 <div class="resume-section-content">
@@ -141,7 +141,7 @@ const Code = ({
                     </div>
                 </div>
             </section>
-        <hr class="m-0" />
+        <hr class="m-0">
             <!-- Education-->
             <section class="resume-section" id="education">
                 <div class="resume-section-content">
@@ -166,7 +166,7 @@ const Code = ({
                     </div>
                 </div>
             </section>
-        <hr class="m-0" />
+        <hr class="m-0">
             <!-- Skills-->
             <section class="resume-section" id="skills">
                 <div class="resume-section-content">
@@ -207,7 +207,7 @@ const Code = ({
                     </ul>
                 </div>
             </section>
-        <hr class="m-0" />
+        <hr class="m-0">
             <!-- Interests-->
             <section class="resume-section" id="interests">
                 <div class="resume-section-content">
@@ -216,7 +216,7 @@ const Code = ({
                     <p class="mb-0">Paragraph-2</p>
                 </div>
             </section>
-        <hr class="m-0" />
+        <hr class="m-0">
             <!-- Awards-->
             <section class="resume-section" id="awards">
                 <div class="resume-section-content">
