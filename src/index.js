b7b2d73bfbf1281dc1bc4175ec10d04232352fbf
Aayush <just.another.3d.object@gmail.com>
2023-03-13 01:29:30 +0530

    Wrapped the app inside Provider tag

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 import "./styles/index.scss";
+import { Provider } from "react-redux";
+import configureStore from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={configureStore}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
