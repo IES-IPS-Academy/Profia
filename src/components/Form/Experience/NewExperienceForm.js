597f6efc660427efaf8c1590b2384a2a99234a36
Aayush <just.another.3d.object@gmail.com>
2023-03-13 02:04:05 +0530

    Created a file for new experience form

---
+++ src/components/Form/Experience/NewExperienceForm.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { createExperience } from "./actions";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+const NewExperienceForm = ({ experiences = [], onCreatePressed }) => {
+  return <div></div>;
+};
