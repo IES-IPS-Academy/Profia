175d09395746d00d3c13bb5b57480a3133b2377c
Aayush <just.another.3d.object@gmail.com>
2023-03-13 02:08:34 +0530

    Added Position section of new experience form

--- src/components/Form/Experience/NewExperienceForm.js
+++ src/components/Form/Experience/NewExperienceForm.js
@@ -4,5 +4,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const NewExperienceForm = ({ experiences = [], onCreatePressed }) => {
-  return <div></div>;
+  const [position, setPosition] = useState("");
+  return (
+    <div className="border rounded bg-light p-3 m-2">
+      <input
+        className="form-control form-control-sm mb-2"
+        type="text"
+        placeholder="Position"
+        value={position}
+        onChange={(e) => setPosition(e.target.value)}
+      />
+    </div>
+  );
 };
